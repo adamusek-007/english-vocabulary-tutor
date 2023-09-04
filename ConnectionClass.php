711047a2c010e1230df00c8024021dd1d435fae3
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-04 10:17:59 +0200

    - Connection fn upgrade

--- ConnectionClass.php
+++ ConnectionClass.php
@@ -6,12 +6,13 @@ class Connector
     {
         static $pdo;
         if (!$pdo) {
-            try {return new PDO(
+            try {$pdo = new PDO(
                 sprintf("mysql:host=%s;dbname=%s;charset=UTF8", DB_HOST, DB_NAME),
                 DB_USER,
                 DB_PASSWORD,
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
             );} catch (PDOException) {
+                echo "Nie można połączyć się z bazą danych";
                 exit;
             }
         }
