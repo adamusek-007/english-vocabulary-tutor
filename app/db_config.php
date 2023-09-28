626759978db96730459ddcca3261149aba004672
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-28 08:04:48 +0200

    Adjusted project to Docker

--- app/db_config.php
+++ app/db_config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'adamusek_007');
-define('DB_USER', 'adamk');
-define('DB_PASSWORD', '[w(7V5ww');
+define('DB_HOST', 'mysql-db');
+define('DB_NAME', 'english_words');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
